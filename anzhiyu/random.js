cfac10eb8f220e7bce8660fb0f4f7df2527af2a0
pour-AImer <2387159@qq.com>
2026-03-02 19:01:01 +0800

    Site updated: 2026-03-02 19:01:01

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/01/Joes with Tiger and Fishes/"];function toRandomPost(){
+var posts=["2026/03/01/Joes-with-Tiger-and-Fishes/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
