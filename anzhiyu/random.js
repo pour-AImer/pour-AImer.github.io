5c062dd089a3319a51ac9cb040d3d123bbc245c0
pour-AImer <2387159@qq.com>
2026-03-02 23:12:32 +0800

    Site updated: 2026-03-02 23:12:31

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2026/03/01/Joes-with-Tiger-and-Fishes/"];function toRandomPost(){
+var posts=["2026/03/01/Joes-with-Tiger-and-Fishes/","2026/03/02/Flowers-and-Alice/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
